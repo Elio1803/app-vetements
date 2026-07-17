427ea1ebe84c84c2049be64f908c60da518786b5
Elio Painteaux <eliopainteaux@MacBook-Air-de-Elio.local>
2026-07-17 10:14:18 +0200

    Fix generate button contrast in dark mode

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'le-dressing-v32'
+const CACHE_NAME = 'le-dressing-v33'
 const resolveFromScope = (path) => new URL(path, self.registration.scope).href
 const APP_SHELL = [
   '',
